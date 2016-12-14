9b57c928b7fd09142d3dcc99040239e8e260d0ce
Vzzzz <VParubets.mail@gmail.com>
2016-12-14 11:15:31 +0700

    Free(): pbstr->lenght = 0;

--- Econophysica/CPP/cpp_test_bstr.cpp
+++ Econophysica/CPP/cpp_test_bstr.cpp
@@ -5,27 +5,27 @@
 
 struct BSTR
 {
-    unsigned int length;
-    wchar_t* pwstring;
+	unsigned int length;
+	wchar_t* pwstring;
 };
 
 void Alloc(BSTR* pbstr, const char* buf)
 {
-    pbstr->length = (unsigned)strlen(buf) * sizeof(wchar_t)/sizeof(char);
-    pbstr->pwstring = new wchar_t(pbstr->length);
-    mbstowcs(pbstr->pwstring, buf, strlen(buf));
+	pbstr->length = (unsigned)strlen(buf) * sizeof(wchar_t)/sizeof(char);
+	pbstr->pwstring = new wchar_t(pbstr->length);
+	mbstowcs(pbstr->pwstring, buf, strlen(buf));
 }
 
 void Free(BSTR* pbstr)
 {
-    delete pbstr->pwstring;
+	pbstr->length = 0;
+	delete pbstr->pwstring;
 }
 
 int main()
 {
-    BSTR* bstring = new BSTR();
-    Alloc(bstring, "Text");
-    Free(bstring);
-    
-    return 0;
+	BSTR* bstring = new BSTR();
+	Alloc(bstring, "Text");
+	Free(bstring);    
+	return 0;
 }
