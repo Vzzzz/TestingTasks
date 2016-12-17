901887e06f0739ccc0a8dc32122e2922b0f8176f
Vzzzz <VParubets.mail@gmail.com>
2016-12-17 15:08:47 +0700

    Comments added to BSTR task

--- Econophysica/CPP/cpp_test_bstr.cpp
+++ Econophysica/CPP/cpp_test_bstr.cpp
@@ -2,16 +2,19 @@
 #include <cstdio>
 #include <cstring>
 
-
+//I defined this struct as it was described
 struct BSTR
 {
 	unsigned int length;
 	wchar_t* pwstring;
 };
 
+//I decided to use BSTR* pbstr to avoid problems with creating an allocated pointer in return block. Just to simplify debugging
 void Alloc(BSTR* pbstr, const char* buf)
 {
+	//In case of *nix (32 bits for unicode character or non-default windows locale)	
 	pbstr->length = (unsigned)strlen(buf) * sizeof(wchar_t)/sizeof(char);
+	//I used new-delete instead of malloc-free only just to notice that this is c++, not pure c code
 	pbstr->pwstring = new wchar_t(pbstr->length);
 	mbstowcs(pbstr->pwstring, buf, strlen(buf));
 }
